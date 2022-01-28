b7d169c528c00a10718fa3279937eb5997d4c480
mariordgez <mariordgez@hotmail.com>
2022-01-27 21:19:59 -0700

    FInished hardhat basic configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,12 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: '0.8.11',
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networs: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
 };
